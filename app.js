a1b0568958bdd2aae57878b87755f7b393c31b65
Abhishek Ranjan <arjha97@gmail.com>
2022-04-05 12:21:43 +0100

    Upload a story form added.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var joinRoomRouter = require('./routes/join-room');
-var postStoryRouter = require('./routes/post-story');
+// var joinRoomRouter = require('./routes/join-room');
+// var postStoryRouter = require('./routes/post-story');
 var app = express();
 
 // view engine setup
@@ -22,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/join-room', joinRoomRouter);
-app.use('/post-story', postStoryRouter);
+// app.use('/join-room', joinRoomRouter);
+// app.use('/post-story', postStoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Browsing' });
+  res.render('index', { title: 'Choose an Option' });
+});
+
+router.get('/join-room', function(req, res, next) {
+  res.render('join-room', { title: 'Join a Room or Create a new one' });
+});
+
+router.get('/post-story', function(req, res, next) {
+  res.render('post-story', { title: 'Upload a Story' });
 });
 
 module.exports = router;
