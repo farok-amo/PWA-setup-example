ad247abaf0155e1f8bdd9380b4ec2201d03b85cc
Abhishek Ranjan <arjha97@gmail.com>
2022-06-03 21:31:36 +0100

    API key changed

--- public/javascripts/chat-room.js
+++ public/javascripts/chat-room.js
@@ -3,6 +3,8 @@ let name = null;
 let roomNo = null;
 let chat = io.connect('/chat');
 
+const apiKey= 'AIzaSyAG7w627q-djB4gTTahssufwNOImRqdYKM';
+
 
 /**
  * called by <body onload>
@@ -108,4 +110,48 @@ function hideLoginInterface(room, userId) {
 
 // const service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
 
+/**
+ * it inits the widget by selecting the type from the field myType
+ * and it displays the Google Graph widget
+ * it also hides the form to get the type
+ */
+
+function widgetInit(){
+    let type= document.getElementById("myType").value;
+
+    if (type) {
+        let config = {
+            'limit': 10,
+            'languages': ['en'],
+            'types': [type],
+            'maxDescChars': 100,
+            'selectHandler': selectItem,
+        }
+        KGSearchWidget(apiKey, document.getElementById("myInput"), config);
+        document.getElementById('typeSet').innerHTML= 'of type: '+type;
+        document.getElementById('widget').style.display='block';
+        document.getElementById('typeForm').style.display= 'none';
+    }
+    else {
+        alert('Set the type please');
+        document.getElementById('widget').style.display='none';
+        document.getElementById('resultPanel').style.display='none';
+        document.getElementById('typeSet').innerHTML= '';
+        document.getElementById('typeForm').style.display= 'block';
+    }
+}
+
+/**
+ * callback called when an element in the widget is selected
+ * @param event the Google Graph widget event {@link https://developers.google.com/knowledge-graph/how-tos/search-widget}
+ */
+function selectItem(event){
+    let row= event.row;
+    // document.getElementById('resultImage').src= row.json.image.url;
+    document.getElementById('resultId').innerText= 'id: '+row.id;
+    document.getElementById('resultName').innerText= row.name;
+    document.getElementById('resultDescription').innerText= row.rc;
+    document.getElementById("resultUrl").href= row.qc;
+    document.getElementById('resultPanel').style.display= 'block';
+}
 
