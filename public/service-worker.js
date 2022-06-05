0b4c8dee699ab24d13e54bd70a1b0c6b9481bbf6
Brendon Rodrigues <99688302+Bren1999@users.noreply.github.com>
2022-06-05 02:03:58 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,23 @@
-// Copyright 2016 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 let cache= null;
-let dataCacheName = 'weatherData-v1';
-let cacheName = 'weatherPWA-step-8-1';
+let dataCacheName = 'storyv2';
+let cacheName = 'v2';
 let filesToCache = [
     '/',
-    '/scripts/app.js',
-    '/styles/inline.css',
-    '/styles/bootstrap.css',
-    '/scripts/bootstrap.js',
-    '/scripts/jquery.min.js',
-    '/scripts/database.js',
-    '/scripts/idb/index.js',
-    '/favicon.ico'
+    '/javascripts/index.js',
+    '/javascripts/canvas.js',
+    '/javascripts/chat-room.js',
+    '/javascripts/database.js',
+    '/javascripts/knowledgeGraph.js',
+    '/javascripts/story.js',
+    '/stylesheets/style.css',
+    '/chat-room/create',
+    '/story/post-story',
+
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+    'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js',
+    '/socket.io/socket.io.js',
+    '/stylesheets/widget.min.css',
+    '/javascripts/widget.min.js',
 ];
 
 
@@ -46,87 +39,97 @@ self.addEventListener('install', function (e) {
 /**
  * activation of service worker: it removes all cashed files if necessary
  */
-self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName && key !== dataCacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-    /*
-     * Fixes a corner case in which the app wasn't returning the latest data.
-     * You can reproduce the corner case by commenting out the line below and
-     * then doing the following steps: 1) load app for first time so that the
-     * initial New York City data is shown 2) press the refresh button on the
-     * app 3) go offline 4) reload the app. You expect to see the newer NYC
-     * data, but you actually see the initial data. This happens because the
-     * service worker is not yet activated. The code below essentially lets
-     * you activate the service worker faster.
-     */
-    return self.clients.claim();
-});
-
-
-/**
- * this is called every time a file is fetched. This is a middleware, i.e. this method is
- * called every time a page is fetched by the browser
- * there are two main branches:
- * /weather_data posts cities names to get data about the weather from the server. if offline, the fetch will fail and the
- *      control will be sent back to Ajax with an error - you will have to recover the situation
- *      from there (e.g. showing the cached data)
- * all the other pages are searched for in the cache. If not found, they are returned
- */
-self.addEventListener('fetch', function (e) {
+ self.addEventListener('fetch', function (e) {
     console.log('[Service Worker] Fetch', e.request.url);
-    let dataUrl = '/weather_data';
-    //if the request is '/weather_data', post to the server - do nit try to cache it
-    if (e.request.url.indexOf(dataUrl) > -1) {
-        /*
-         * When the request URL contains dataUrl, the app is asking for fresh
-         * weather data. In this case, the service worker always goes to the
-         * network and then caches the response. This is called the "Cache then
-         * network" strategy:
-         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-         */
-        return fetch(e.request)
-            .then( (response) => {
-                // note: it the network is down, response will contain the error
-                // that will be passed to Ajax
-                return response;
-            })
-            .catch((error) => {
-                return error;
-            })
-    } else {
-        /*
-         * The app is asking for app shell files. In this scenario the app uses the
-         * "Cache, falling back to the network" offline strategy:
-         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-         */
-        e.respondWith(
-            caches.match(e.request).then(function (response) {
-                return response
-                    || fetch(e.request)
-                        .then(function (response) {
-                            // note if network error happens, fetch does not return
-                            // an error. it just returns response not ok
-                            // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
-                            if (!response.ok ||  response.statusCode>299) {
-                                console.log("error: " + response.error());
-                            } else {
-                                cache.add(e.request.url);
-                                return response;
-                            }
-                        })
-                        .catch(function (err) {
-                            console.log("error: " + err);
-                        })
-            })
-        );
-    }
-});
+    /*
+    * The app is asking for app shell files. In this scenario the app uses the
+    * "Cache, falling back to the network" offline strategy:
+    * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+    */
+
+      
+
+      if (/\/chat-room\/create?.+/g.exec(e.request.url)){
+        // Return join page
+        console.log(`[Service Worker] Request Create-Room page`);
+        e.respondWith(async function() {
+          try {
+            response = await fetch(e.request);
+            console.log(`[Service Worker] Fetch Create-Room`);
+            return response;
+          } catch (error) {
+            console.log(`[Service Worker] Fetch Offline Create-Room`);
+            cashed = await caches.match(e.request)
+            return cashed;
+          }
+        }());
+        return;
+      }
+       if (e.request.url.indexOf('chrome-extension') == 0){
+        
+        
+        console.log('[Service Worker] re-route //chrome-extension', e.request.url);
+        e.respondWith(fetch(e.request));
+
+        return;
+      }
+    
+      
+      if (e.request.url.indexOf('socket.io/') > -1){
+    
+        console.log('[Service Worker] re-route socket.io', e.request.url);
+        e.respondWith(fetch(e.request));
+
+        return;
+      }
+
+      if (e.request.url.indexOf('kgsearch.googleapis.com') == 0){
+
+        console.log('[Service Worker] re-route kgsearch.googleapis.com', e.request.url);
+        e.respondWith(fetch(e.request));
+        return;
+      }
+     // e.respondWith(
+     //     caches.match(e.request).then(function (response) {
+     //         return response
+     //             || fetch(e.request)
+     //                 .then(function (response) {
+     //                     // note if network error happens, fetch does not return
+     //                     // an error. it just returns response not ok
+     //                     // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+     //                     if (!response.ok ||  response.statusCode>299) {
+     //                         console.log("error: " + response.error());
+     //                     } else {
+     //                         cache.add(e.request.url);
+     //                         return response;
+     //                     }
+     //                 })
+     //                 .catch(function (err) {
+     //                     console.log("error: " + err);
+     //                 })
+     //     })
+     // );
+      e.respondWith(async function () {
+        response = await caches.match(e.request);
+
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        response = await fetch(e.request);
+
+        // Response validation
+        if (!response || response.status !== 200) {
+          console.log(`Response error: [${response.status}]: ${response.statusText}`);
+          return response
+        }
+
+        // Store to the cache
+        var responseToCache = response.clone();
+        cache = await caches.open(cacheName)
+        cache.put(e.request, responseToCache);
+
+        return response;
+      }());
+    })
