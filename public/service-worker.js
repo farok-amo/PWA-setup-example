bba448878b3b7dcd1fb106d0f84cee71696af54e
farok-amo <farok.amo@gmail.com>
2022-06-05 20:16:08 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,21 +24,24 @@ let filesToCache = [
 /**
  * installation event: it adds all the files to be cached
  */
-self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function (cacheX) {
-            console.log('[ServiceWorker] Caching app shell');
-            cache= cacheX;
-            return cache.addAll(filesToCache);
-        })
-    );
+ self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+      caches.keys().then(function (keyList) {
+          return Promise.all(keyList.map(function (key) {
+              if (key !== cacheName && key !== dataCacheName) {
+                  console.log('[ServiceWorker] Removing old cache', key);
+                  return caches.delete(key);
+              }
+          }));
+      })
+  );
+
+  //return self.clients.claim();
 });
 
 
-/**
- * activation of service worker: it removes all cashed files if necessary
- */
+
  self.addEventListener('fetch', function (e) {
     console.log('[Service Worker] Fetch', e.request.url);
     /*
@@ -89,47 +92,47 @@ self.addEventListener('install', function (e) {
         e.respondWith(fetch(e.request));
         return;
       }
-     // e.respondWith(
-     //     caches.match(e.request).then(function (response) {
-     //         return response
-     //             || fetch(e.request)
-     //                 .then(function (response) {
-     //                     // note if network error happens, fetch does not return
-     //                     // an error. it just returns response not ok
-     //                     // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
-     //                     if (!response.ok ||  response.statusCode>299) {
-     //                         console.log("error: " + response.error());
-     //                     } else {
-     //                         cache.add(e.request.url);
-     //                         return response;
-     //                     }
-     //                 })
-     //                 .catch(function (err) {
-     //                     console.log("error: " + err);
-     //                 })
-     //     })
-     // );
-      e.respondWith(async function () {
-        response = await caches.match(e.request);
+     e.respondWith(
+         caches.match(e.request).then(function (response) {
+             return response
+                 || fetch(e.request)
+                     .then(function (response) {
+                         // note if network error happens, fetch does not return
+                         // an error. it just returns response not ok
+                         // https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+                         if (!response.ok ||  response.statusCode>299) {
+                             console.log("error: " + response.error());
+                         } else {
+                             cache.add(e.request.url);
+                             return response;
+                         }
+                     })
+                     .catch(function (err) {
+                         console.log("error: " + err);
+                     })
+         })
+     );
+      // e.respondWith(async function () {
+      //   response = await caches.match(e.request);
 
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+      //   // Cache hit - return response
+      //   if (response) {
+      //     return response;
+      //   }
 
-        response = await fetch(e.request);
+      //   response = await fetch(e.request);
 
-        // Response validation
-        if (!response || response.status !== 200) {
-          console.log(`Response error: [${response.status}]: ${response.statusText}`);
-          return response
-        }
+      //   // Response validation
+      //   if (!response || response.status !== 200) {
+      //     console.log(`Response error: [${response.status}]: ${response.statusText}`);
+      //     return response
+      //   }
 
-        // Store to the cache
-        var responseToCache = response.clone();
-        cache = await caches.open(cacheName)
-        cache.put(e.request, responseToCache);
+      //   // Store to the cache
+      //   var responseToCache = response.clone();
+      //   cache = await caches.open(cacheName)
+      //   cache.put(e.request, responseToCache);
 
-        return response;
-      }());
+      //   return response;
+      // }());
     })
