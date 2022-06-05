720bd9a1a3fd67d13233ecd8104faf78bfa11306
farok-amo <farok.amo@gmail.com>
2022-06-05 23:56:13 +0100

    SW fixes

---
+++ socket.io/socket.io.js
@@ -0,0 +1,48 @@
+
+exports.init = function(io) {
+
+  const chat = io
+      .of('/chat')
+      .on('connection', (socket) => {
+        try{
+          socket.on('create or join', (room, userId) => {
+            socket.join(room);
+            chat.to(room).emit('joined', room, userId);
+          });
+
+          socket.on('chat', (room, userId, chatText) => {
+            chat.to(room).emit('chat', room, userId, chatText);
+          });
+
+          socket.on('disconnect', () => {
+            console.log('Disconnected');
+          });
+
+          socket.on('drawing', function (room, userId, canvasWidth, canvasHeight, x1, y21, x2, y2, color, thickness) {
+                chat.to(room).emit('drawing', room, userId, canvasWidth, canvasHeight, x1, y21, x2, y2, color, thickness)
+          });
+
+        } catch (e) {
+
+        }
+      });
+  /*io.sockets.on('connection', function (socket) {
+    try {
+     // insert here your event
+      socket.on('create or join', (room, userId) => {
+        socket.join(room);
+        io.sockets.to(room).emit('joined', room, userId);
+      });
+
+      socket.on('chat', (room, userId, chatText) => {
+        io.sockets.to(room).emit('chat', room, userId, chatText);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Disconnected');
+      });
+    } catch (e) {
+
+    }
+  });*/
+}
